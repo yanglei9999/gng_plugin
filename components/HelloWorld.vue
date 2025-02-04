448600f1a959a3e82441ecf7fa93dce683870fd7
yanglei <715860993@qq.com>
2025-02-04 13:07:57 +0800

    全局注册的方式载入 antd Vue

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -10,6 +10,13 @@ const count = ref(0);
 
 <template>
   <h1>{{ msg }}</h1>
+  <a-space wrap>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </a-space>
 </template>
 
 <style scoped>
